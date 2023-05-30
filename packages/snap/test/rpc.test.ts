ce9ca2c46dd57b78c6f7b72b950632a93eb27d5b
Frederik Zwilling <frederik.zwilling@gmail.com>
2023-05-30 18:33:05 +0200

    new prover files for tests including shamir's secret sharing

--- packages/snap/test/rpc.test.ts
+++ packages/snap/test/rpc.test.ts
@@ -6,7 +6,7 @@ import { groth16 } from 'snarkjs';
 
 import zkCert from '../../../test/zkCert.json';
 import zkCert2 from '../../../test/zkCert2.json';
-import ageProofVKey from '../../galactica-dapp/public/provers/ageProofZkKYC.vkey.json';
+import exampleMockDAppVKey from '../../galactica-dapp/public/provers/exampleMockDApp.vkey.json';
 import { processRpcRequest } from '../src';
 import { RpcMethods, RpcResponseErr, RpcResponseMsg } from '../src/rpcEnums';
 import { ExportRequestParams, RpcArgs, ZkCertProof } from '../src/types';
@@ -53,7 +53,7 @@ async function verifyProof(result: ZkCertProof) {
   expect(result.publicSignals.length).to.be.gt(5);
 
   const verification = await groth16.verify(
-    ageProofVKey,
+    exampleMockDAppVKey,
     result.publicSignals,
     result.proof,
   );
