5e5eb84fc660dcf8284c0a20c10ffb3b475ef0b0
Frederik Zwilling <frederik.zwilling@gmail.com>
2023-09-26 16:09:34 +0200

    CI: zk circuit build cache setup

--- packages/zk-certificates/tasks/circomTemplate.ts
+++ packages/zk-certificates/tasks/circomTemplate.ts
@@ -1,19 +1,29 @@
-import { TASK_CIRCOM_TEMPLATE } from "hardhat-circom";
-import { subtask } from "hardhat/config";
-import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { subtask } from 'hardhat/config';
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { TASK_CIRCOM_TEMPLATE } from 'hardhat-circom';
 
-subtask(TASK_CIRCOM_TEMPLATE, "generate Verifier template shipped by SnarkjS").setAction(circomTemplate);
+subtask(
+  TASK_CIRCOM_TEMPLATE,
+  'generate Verifier template shipped by SnarkjS',
+).setAction(circomTemplate);
 
 /**
  * Overwrite the default circomTemplate task to change the destination path of Verifier contracts.
- * 
+ *
  * @param args - Task arguments.
  * @param hre - Hardhat runtime environment.
  * @param runSuper - Super function provided by snarkjs.
  */
-async function circomTemplate(args: any, hre: HardhatRuntimeEnvironment, runSuper: any) {
+async function circomTemplate(
+  args: any,
+  hre: HardhatRuntimeEnvironment,
+  runSuper: any,
+) {
   const previousPath = hre.config.paths.sources;
-  hre.config.paths.sources = hre.config.paths.sources + "/zkpVerifiers";
+  hre.config.paths.sources = `${hre.config.paths.sources}/zkpVerifiers`;
   await runSuper(args);
+
+  // reset the path
+  // eslint-disable-next-line require-atomic-updates
   hre.config.paths.sources = previousPath;
 }
