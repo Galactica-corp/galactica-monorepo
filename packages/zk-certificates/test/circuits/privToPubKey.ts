6ad880160f746600835949961bd7beb163c353eb
Frederik Zwilling <frederik.zwilling@gmail.com>
2023-11-23 08:57:14 +0100

    types: type alias for EdDSA keys

--- packages/zk-certificates/test/circuits/privToPubKey.ts
+++ packages/zk-certificates/test/circuits/privToPubKey.ts
@@ -1,6 +1,6 @@
 /* Copyright (C) 2023 Galactica Network. This file is part of zkKYC. zkKYC is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. zkKYC is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>. */
 import { assert } from 'chai';
-import { buildEddsa } from 'circomlibjs';
+import { Eddsa, buildEddsa } from 'circomlibjs';
 import { readFileSync } from 'fs';
 import hre, { ethers } from 'hardhat';
 import { CircuitTestUtils } from 'hardhat-circom';
@@ -12,7 +12,7 @@ import {
 
 describe('Private to public key derivation', () => {
   let circuit: CircuitTestUtils;
-  let eddsa: any;
+  let eddsa: Eddsa;
 
   const sampleInput = JSON.parse(
     readFileSync('./circuits/input/privToPubKey.json', 'utf8'),
--- packages/zk-certificates/test/lib/shamirTools.ts
+++ packages/zk-certificates/test/lib/shamirTools.ts
@@ -1,11 +1,11 @@
 /* Copyright (C) 2023 Galactica Network. This file is part of zkKYC. zkKYC is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. zkKYC is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>. */
 import { expect } from 'chai';
-import { buildEddsa } from 'circomlibjs';
+import { Eddsa, buildEddsa } from 'circomlibjs';
 
 import { reconstructShamirSecret } from '../../lib/shamirTools';
 
 describe('Shamir Tools', () => {
-  let eddsa: any;
+  let eddsa: Eddsa;
 
   before(async () => {
     eddsa = await buildEddsa();
