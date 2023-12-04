839aabcc4f05540e87cfb44ad522468fc6d53892
Frederik Zwilling <frederik.zwilling@gmail.com>
2023-12-04 13:07:46 +0100

    zkCert: adjusted script getMerkleProofFromChain to recent refactoring

--- packages/zk-certificates/scripts/getMerkleProofFromChain.ts
+++ packages/zk-certificates/scripts/getMerkleProofFromChain.ts
@@ -1,57 +1,46 @@
-import { buildPoseidon } from 'circomlibjs';
 import fs from 'fs';
 import { ethers } from 'hardhat';
 import path from 'path';
 
 import { printProgress } from '../lib/helpers';
-import { queryOnChainLeaves } from '../lib/queryMerkleTree';
-import { SparseMerkleTree } from '../lib/sparseMerkleTree';
+import { buildMerkleTreeFromRegistry } from '../lib/queryMerkleTree';
 
 /**
  * Script for creating a merkle tree for testing from a list of UTXOs, benchmark version.
  */
 async function main() {
+  // input
   const registryAddress = '0xAbb654092b5BCaeca2E854550c5C972602eF7dA8';
   const leavesToProve = [
     '1587890648226949363967927358747281515927759982145649624939771519342787074806',
   ];
-
-  // Create a new poseidon instance for hashing
-  const poseidon = await buildPoseidon();
-
-  // input
   const merkleDepth = 32;
 
-  // build merkle tree
-  const merkleTree = new SparseMerkleTree(merkleDepth, poseidon);
-  const leafLogResults = await queryOnChainLeaves(
-    ethers,
+  const registry = await ethers.getContractAt(
+    'KYCRecordRegistry',
     registryAddress,
+  );
+
+  // build merkle tree
+  const merkleTree = await buildMerkleTreeFromRegistry(
+    registry,
+    ethers.provider,
+    merkleDepth,
     1,
     printProgress,
   );
-  const leafHashes = leafLogResults.map((log) => log.leafHash);
-  const leafIndices = leafLogResults.map((log) => Number(log.index));
-  const batchSize = 10_000;
-  for (let i = 0; i < leafLogResults.length; i += batchSize) {
-    merkleTree.insertLeaves(
-      leafHashes.slice(i, i + batchSize),
-      leafIndices.slice(i, i + batchSize),
-    );
-  }
-
   // console.log(`Merkle leaves: ${merkleTree.tree[0]}`);
 
   // create Merkle proofs
   for (const leaf of leavesToProve) {
-    const leafIndex = leafIndices[leafHashes.indexOf(leaf)];
+    const leafIndex = merkleTree.getLeafIndex(leaf);
     const merkleProof = merkleTree.createProof(leafIndex);
 
     const output = {
       leaf,
       root: merkleTree.root,
       leafIndex: merkleProof.leafIndex,
-      pathElements: merkleProof.path,
+      pathElements: merkleProof.pathElements,
     };
 
     console.log(`Merkle proof for ${leaf}:\n`, JSON.stringify(output, null, 2));
