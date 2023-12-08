8a6548d70d01871e9a747ffc80b301c2670ce480
alex-abanin <aa@occam.fi>
2023-12-08 10:21:43 +0000

    config updated

--- packages/snap-api/src/config/index.ts
+++ packages/snap-api/src/config/index.ts
@@ -7,8 +7,9 @@ export type SdkConfig = {
     exampleZkpVerifier: string;
     exampleDapp: string;
     exampleInstitution: string[];
-    basicKycExampleDapp: string;
-    repeatableZkKycTest: string;
+    exampleZkKyc: string;
+    repeatableZkpTest: string;
+    devnetGuardian: string;
   };
   defaultSnapOrigin: string;
   defaultSnapVersion: string;
@@ -16,19 +17,20 @@ export type SdkConfig = {
 
 export const sdkConfig: SdkConfig = {
   contracts: {
-    kycGuardianWhitelist: '0x4De49e2047eE726B833fa815bf7392958245832d',
-    zkKycRegistry: '0x8eD8311ED65eBe2b11ED8cB7076E779c1030F9cF',
-    verificationSbt: '0xc1a96F7DD532fa4B774C41f9Eb853893314cB036',
-    exampleZkKycAgeProof: '0x7790dDa9E7569bc3580E675D75Ad115E7B35c6ff',
-    exampleZkpVerifier: '0xf1947AeD2d0a5Ff90D54b63C85904d258D3B5E63',
-    exampleDapp: '0xf28CFA74C8f1298aaE8231dD10D3594dFB0D6201',
+    kycGuardianWhitelist: '0xB95314E42d8Da05b1D805039F7b96e3935584543',
+    zkKycRegistry: '0x0415E990e55071F0d448F87CD170528C7783A484',
+    verificationSbt: '0x062DaB74A2709EC730FE11b3d9C00033B0FBAf92',
+    exampleZkKycAgeProof: '0x1554B2D7422941Be9903040d9946da60d9f2bC3C',
+    exampleZkpVerifier: '0xA437DFE87F9096e52E4dEcAA7cfc14d82A5aE07e',
+    exampleDapp: '0xBD2FAA4835E1A462Ea4FDA39352F24C58e237e2a',
     exampleInstitution: [
-      '0x9b48274258501C54E9ac7B165B391bf2b6E863EE',
-      '0x8d2a748ebC5EbD2F96BAb1De576CA70dbdFa336B',
-      '0x9d862c70a0e8726cce50308FE0194652Fb078739',
+      '0x5E523B1c7c04eB971D3b851C82d3935b91fb4b3e',
+      '0xcc433436B6c01142EC9B95a1b80B3734CD4849f3',
+      '0x8d5ebACB647aea8a76082A36C93997765A50AF0E',
     ],
-    basicKycExampleDapp: '0x02ee87FE35cF9e635B752671D5477516256aDB4f',
-    repeatableZkKycTest: '0x57ebf246fC38c59f48CE316381eEFF883C006Fa1',
+    exampleZkKyc: '0xD95efF72F06079DEcE33b18B165fc3A7a4bdc1fD',
+    repeatableZkpTest: '0xa99fcD678D985fB2ac8aD3fE913aED88705A44fc',
+    devnetGuardian: '0x71d80ea7744302E5b1cFD61a7a26153FF221ca9E',
   },
   defaultSnapOrigin: 'npm:@galactica-net/snap',
   defaultSnapVersion: '0.6.1',
