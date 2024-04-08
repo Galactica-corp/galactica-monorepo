3eb88f7f0681eb1d34ba028592f28323a4658d24
Frederik Zwilling <frederik.zwilling@gmail.com>
2024-04-08 15:35:04 +0200

    test-frontend: resolved errors by adding 404 page

---
+++ packages/galactica-dapp/src/pages/404.tsx
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex: 1;
+  margin-top: 7.6rem;
+  margin-bottom: 7.6rem;
+  ${({ theme }) => theme.mediaQueries.small} {
+    padding-left: 2.4rem;
+    padding-right: 2.4rem;
+    margin-top: 2rem;
+    margin-bottom: 2rem;
+    width: auto;
+  }
+`;
+
+const Heading = styled.h1`
+  margin-top: 0;
+  margin-bottom: 2.4rem;
+  text-align: center;
+`;
+
+const Index = () => {
+
+  return (
+    <Container>
+      <Heading>
+        404: Page not found
+      </Heading>
+    </Container>
+  );
+};
+
+export default Index;
