2f1dbd010cb83d76c6dc4fedbe207d822cab3f05
anhdungle <anhdungle634@gmail.com>
2024-08-31 16:09:23 +0700

    add KZG ts functions

---
+++ packages/zk-certificates/contracts/KZGVerifier.sol
@@ -0,0 +1,257 @@
+// Modified from https://github.com/appliedzkp/semaphore/blob/master/contracts/sol/verifier.sol
+
+pragma solidity ^0.8.0;
+
+library Pairing {
+
+    uint256 constant PRIME_Q = 21888242871839275222246405745257275088696311157297823662689037894645226208583;
+
+    struct G1Point {
+        uint256 X;
+        uint256 Y;
+    }
+
+    // Encoding of field elements is: X[0] * z + X[1]
+    struct G2Point {
+        uint256[2] X;
+        uint256[2] Y;
+    }
+
+    /*
+     * @return The negation of p, i.e. p.plus(p.negate()) should be zero.
+     */
+    function negate(G1Point memory p) internal pure returns (G1Point memory) {
+
+        // The prime q in the base field F_q for G1
+        if (p.X == 0 && p.Y == 0) {
+            return G1Point(0, 0);
+        } else {
+            return G1Point(p.X, PRIME_Q - (p.Y % PRIME_Q));
+        }
+    }
+
+    /*
+     * @return The sum of two points of G1
+     */
+    function plus(
+        G1Point memory p1,
+        G1Point memory p2
+    ) internal view returns (G1Point memory r) {
+
+        uint256[4] memory input;
+        input[0] = p1.X;
+        input[1] = p1.Y;
+        input[2] = p2.X;
+        input[3] = p2.Y;
+        bool success;
+
+        // solium-disable-next-line security/no-inline-assembly
+        assembly {
+            success := staticcall(sub(gas(), 2000), 6, input, 0xc0, r, 0x60)
+            // Use "invalid" to make gas estimation work
+            switch success case 0 { invalid() }
+        }
+
+        require(success, "pairing-add-failed");
+    }
+
+    /*
+     * @return The product of a point on G1 and a scalar, i.e.
+     *         p == p.scalar_mul(1) and p.plus(p) == p.scalar_mul(2) for all
+     *         points p.
+     */
+    function mulScalar(G1Point memory p, uint256 s) internal view returns (G1Point memory r) {
+
+        uint256[3] memory input;
+        input[0] = p.X;
+        input[1] = p.Y;
+        input[2] = s;
+        bool success;
+        // solium-disable-next-line security/no-inline-assembly
+        assembly {
+            success := staticcall(sub(gas(), 2000), 7, input, 0x80, r, 0x60)
+            // Use "invalid" to make gas estimation work
+            switch success case 0 { invalid() }
+        }
+        require (success, "pairing-mul-failed");
+    }
+
+    /* @return The result of computing the pairing check
+     *         e(p1[0], p2[0]) *  .... * e(p1[n], p2[n]) == 1
+     *         For example,
+     *         pairing([P1(), P1().negate()], [P2(), P2()]) should return true.
+     */
+    function pairing(
+        G1Point memory a1,
+        G2Point memory a2,
+        G1Point memory b1,
+        G2Point memory b2
+    ) internal view returns (bool) {
+
+        G1Point[2] memory p1 = [a1, b1];
+        G2Point[2] memory p2 = [a2, b2];
+
+        uint256 inputSize = 12;
+        uint256[] memory input = new uint256[](inputSize);
+
+        for (uint256 i = 0; i < 2; i++) {
+            uint256 j = i * 6;
+            input[j + 0] = p1[i].X;
+            input[j + 1] = p1[i].Y;
+            input[j + 2] = p2[i].X[0];
+            input[j + 3] = p2[i].X[1];
+            input[j + 4] = p2[i].Y[0];
+            input[j + 5] = p2[i].Y[1];
+        }
+
+        uint256[1] memory out;
+        bool success;
+
+        // solium-disable-next-line security/no-inline-assembly
+        assembly {
+            success := staticcall(sub(gas(), 2000), 8, add(input, 0x20), mul(inputSize, 0x20), out, 0x20)
+            // Use "invalid" to make gas estimation work
+            switch success case 0 { invalid() }
+        }
+
+        require(success,"pairing-opcode-failed");
+
+        return out[0] != 0;
+    }
+}
+
+contract KZGVerifier {
+
+    using Pairing for *;
+
+    // order of the group
+    uint256 constant BABYJUB_P = 21888242871839275222246405745257275088548364400416034343698204186575808495617;
+
+    Pairing.G2Point g2Generator = Pairing.G2Point({
+        X: [
+            uint256(0x198e9393920d483a7260bfb731fb5d25f1aa493335a9e71297e485b7aef312c2),
+            uint256(0x1800deef121f1e76426a00665e5c4479674322d4f75edadd46debd5cd992f6ed)
+        ],
+        Y: [
+            uint256(0x090689d0585ff075ec9e99ad690c3395bc4b313370b38ef355acdadcd122975b),
+            uint256(0x12c85ea5db8c6deb4aab71808dcb408fe3d1e7690c43d37b4ce6cc0166fa7daa)
+        ]
+
+    });
+
+    Pairing.G1Point SRS_G1_0 = Pairing.G1Point({ X: 1, Y: 2 });
+
+    /*
+     * This value is the second TauG2 value from challenge file #46 of the
+     * Perpetual Powers of Tau ceremony. The Blake2b hash of challenge file
+     * is:
+     *
+     * 939038cd 2dc5a1c0 20f368d2 bfad8686
+     * 950fdf7e c2d2e192 a7d59509 3068816b
+     * becd914b a293dd8a cb6d18c7 b5116b66
+     * ea54d915 d47a89cc fbe2d5a3 444dfbed
+     *
+     * The challenge file can be retrieved at:
+     * https://ppot.blob.core.windows.net/public/challenge_0046
+     *
+     * The ceremony transcript can be retrieved at:
+     * https://github.com/weijiekoh/perpetualpowersoftau
+     *
+     * Anyone can verify the transcript to ensure that the values in the challenge
+     * file have not been tampered with. Moreover, as long as one participant in
+     * the ceremony has discarded their toxic waste, the whole ceremony is secure.
+     * Please read the following for more information:
+     * https://medium.com/coinmonks/announcing-the-perpetual-powers-of-tau-ceremony-to-benefit-all-zk-snark-projects-c3da86af8377
+     */
+    Pairing.G2Point SRS_G2_1 = Pairing.G2Point({
+        X: [
+            uint256(0x21a808dad5c50720fb7294745cf4c87812ce0ea76baa7df4e922615d1388f25a),
+            uint256(0x04c5e74c85a87f008a2feb4b5c8a1e7f9ba9d8eb40eb02e70139c89fb1c505a9)
+        ],
+        Y: [
+            uint256(0x204b66d8e1fadc307c35187a6b813be0b46ba1cd720cd1c4ee5f68d13036b4ba),
+            uint256(0x2d58022915fc6bc90e036e858fbc98055084ac7aff98ccceb0e3fde64bc1a084)
+        ]
+    });
+
+    /*
+     * Verifies a single-point evaluation of a polynominal using the KZG
+     * commitment scheme.
+     *    - p(X) is a polynominal
+     *    - _value = p(_index)
+     *    - commitment = commit(p)
+     *    - proof = genProof(p, _index, _value)
+     * Returns true if and only if the following hold:
+     *     - e(commitment - commit([_value]), G2.g) == e(proof, commit([0, 1]) - zCommit)
+     * @param _commitmentX The X-coordinate of the commitment.
+     * @param _commitmentY The Y-coordinate of the commitment.
+     * @param _proofX The X-coordinate of the proof.
+     * @param _proofY The Y-coordinate of the proof.
+     * @param _index The x-value at which to evaluate the polynominal.
+     * @param _value The result of the polynominal evaluation.
+     */
+    function verifyKZG(
+        uint256 _commitmentX,
+        uint256 _commitmentY,
+        uint256 _proofX,
+        uint256 _proofY,
+        uint256 _index,
+        uint256 _value
+    ) public view returns (bool) {
+        // Make sure each parameter is less than the prime q
+        require(_commitmentX < BABYJUB_P, "Verifier.verifyKZG: commitmentX is out of range");
+        require(_commitmentY < BABYJUB_P, "Verifier.verifyKZG: commitmentY is out of range");
+        require(_proofX < BABYJUB_P, "Verifier.verifyKZG: proofX is out of range");
+        require(_proofY < BABYJUB_P, "Verifier.verifyKZG: proofY is out of range");
+        require(_index < BABYJUB_P, "Verifier.verifyKZG: index is out of range");
+        require(_value < BABYJUB_P, "Verifier.verifyKZG: value is out of range");
+
+        // Check that
+        //     e(commitment - aCommit, G2.g) == e(proof, xCommit - zCommit)
+        //     e(commitment - aCommit, G2.g) / e(proof, xCommit - zCommit) == 1
+        //     e(commitment - aCommit, G2.g) * e(proof, xCommit - zCommit) ^ -1 == 1
+        //     e(commitment - aCommit, G2.g) * e(-proof, xCommit - zCommit) == 1
+        // where:
+        //     aCommit = commit([_value]) = SRS_G1_0 * _value
+        //     xCommit = commit([0, 1]) = SRS_G2_1
+        //     zCommit = commit([_index]) = SRS_G2_1 * _index
+
+        // To avoid having to perform an expensive operation in G2 to compute
+        // xCommit - zCommit, we instead check the equivalent equation:
+        //     e(commitment - aCommit, G2.g) * e(-proof, xCommit) * e(-proof, -zCommit) == 1
+        //     e(commitment - aCommit, G2.g) * e(-proof, xCommit) * e(proof, zCommit) == 1
+        //     e(commitment - aCommit, G2.g) * e(-proof, xCommit) * e(index * proof, G2.g) == 1
+        //     e((index * proof) + (commitment - aCommit), G2.g) * e(-proof, xCommit) == 1
+
+
+        // Compute commitment - aCommitment
+        Pairing.G1Point memory commitmentMinusA = Pairing.plus(
+            Pairing.G1Point({ X: _commitmentX, Y: _commitmentY }),
+            Pairing.negate(
+                Pairing.mulScalar(SRS_G1_0, _value)
+            )
+        );
+
+        // The proof as a G1 point
+        Pairing.G1Point memory proofPoint =
+            Pairing.G1Point({
+                X: _proofX,
+                Y: _proofY
+            });
+
+        // Negate the proof
+        Pairing.G1Point memory negProof = Pairing.negate(proofPoint);
+
+        // Compute index * proof
+        Pairing.G1Point memory indexMulProof = Pairing.mulScalar(proofPoint, _index);
+
+        // Returns true if and only if
+        // e((index * proof) + (commitment - aCommitment), G2.g) * e(-proof, xCommit) == 1
+        return Pairing.pairing(
+            Pairing.plus(indexMulProof, commitmentMinusA),
+            g2Generator,
+            negProof,
+            SRS_G2_1
+        );
+    }
+}
---
+++ packages/zk-certificates/contracts/ZkCertificateRegistryVT.sol
@@ -0,0 +1,383 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.7;
+pragma abicoder v2;
+
+// OpenZeppelin v4
+import {Initializable} from '@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol';
+
+import {SNARK_SCALAR_FIELD} from './helpers/Globals.sol';
+
+import {PoseidonT3} from './helpers/Poseidon.sol';
+
+import {GuardianRegistry, GuardianInfo} from './GuardianRegistry.sol';
+
+import {IZkCertificateRegistry} from './interfaces/IZkCertificateRegistry.sol';
+
+import {Ownable} from './Ownable.sol';
+
+import {Pairing, KZGVerifier} from './KZGVerifier.sol';
+
+/**
+ * @title ZkCertificateRegistry
+ * @author Galactica dev team
+ * @notice Sparse Merkle Tree for revokable ZK certificates records
+ * Relevant external contract calls should be in those functions, not here
+ */
+contract ZkCertificateRegistryVT is Initializable, IZkCertificateRegistry, Ownable, KZGVerifier {
+    // NOTE: The order of instantiation MUST stay the same across upgrades
+    // add new variables to the bottom of the list and decrement the __gap
+    // variable at the end of this file
+    // See https://docs.openzeppelin.com/learn/upgrading-smart-contracts#upgrading
+
+    // a short description to describe the zkCertificate we store in this SC
+    // examples: zkKYC, Twitter zkCertificate
+    string public description;
+
+    // The tree depth and size
+    uint256 public immutable treeDepth;
+    uint256 public immutable treeSize;
+    uint256 public immutable leafWidth;
+
+    // Tree zero value
+    bytes32 public constant ZERO_VALUE =
+        bytes32(uint256(keccak256('Galactica')) % SNARK_SCALAR_FIELD);
+
+    // Next leaf index (number of inserted leaves in the current tree)
+    uint256 public nextLeafIndex;
+
+    // array of all merkle roots
+    bytes32[] public merkleRoots;
+    // and from which index the merkle roots are still valid
+    // we start from 1 because nonexistant merkle roots return 0 in the merkleRootIndex mapping
+    uint256 public merkleRootValidIndex = 1;
+    // we will also store the merkle root index in a mapping for quicker lookup
+    mapping(bytes32 => uint) public merkleRootIndex;
+
+    // Block height at which the contract was initialized
+    // You can use it to speed up finding all logs of the contract by starting from this block
+    uint256 public initBlockHeight;
+
+    // a mapping to store which Guardian manages which ZkCertificate
+    mapping(bytes32 => address) public ZkCertificateToGuardian;
+    uint256 public queueExpirationTime = 60*60; // Guardian has at least one hour to add ZkCertificate after registration to the queue
+    bytes32[] public ZkCertificateQueue;
+    uint256 public currentQueuePointer;
+    mapping(bytes32 => uint) public ZkCertificateHashToIndexInQueue;
+    mapping(bytes32 => uint) public ZkCertificateHashToQueueTime;
+    mapping(bytes32 => address) public ZkCertificateHashToCommitedGuardian;
+
+    GuardianRegistry public _GuardianRegistry;
+    event zkCertificateAddition(
+        bytes32 indexed zkCertificateLeafHash,
+        address indexed Guardian,
+        uint index
+    );
+    event zkCertificateRevocation(
+        bytes32 indexed zkCertificateLeafHash,
+        address indexed Guardian,
+        uint index
+    );
+
+    constructor(
+        address GuardianRegistry_,
+        uint256 treeDepth_,
+        uint256 leafWidth_,
+        string memory description_
+    ) initializer Ownable(msg.sender){
+        treeDepth = treeDepth_;
+        treeSize = 2 ** treeDepth;
+        leafWidth = leafWidth_;
+        initializeZkCertificateRegistry(GuardianRegistry_, description_);
+    }
+
+    /**
+     * @notice return the current merkle root which is the last one in the merkleRoots array
+     */
+    function merkleRoot() public view returns (bytes32) {
+        return merkleRoots[merkleRoots.length - 1];
+    }
+
+    /**
+     * @notice return the whole merkle root array
+     */
+    function getMerkleRoots() public view returns (bytes32[] memory) {
+        return merkleRoots;
+    }
+
+    /**
+     * @notice Calculates initial values for Merkle Tree
+     * @dev OpenZeppelin initializer ensures this can only be called once
+     */
+    function initializeZkCertificateRegistry(
+        address GuardianRegistry_,
+        string memory description_
+    ) internal onlyInitializing {
+        description = description_;
+        /*
+        To initialize the Merkle tree, we need to calculate the Merkle root
+        assuming that each leaf is the zero value.
+        H(H(a,b), H(c,d))
+        /          \
+        H(a,b)     H(c,d)
+        /   \       /  \
+        a    b     c    d
+        */
+
+        // Store the current zero value for the level we just calculated it for
+        bytes32 currentZero = ZERO_VALUE;
+
+        // Loop through each level
+        for (uint256 i = 0; i < treeDepth; i += 1) {
+            // Calculate the zero value for this level
+            currentZero = hashLeftRight(currentZero, currentZero);
+        }
+
+        // Set merkle root
+        merkleRoots.push(currentZero);
+        _GuardianRegistry = GuardianRegistry(GuardianRegistry_);
+
+        // Set the block height at which the contract was initialized
+        initBlockHeight = block.number;
+    }
+    /**
+     * @notice Change the time until which the Guardian needs to add/revoke the zkCertificate after registration to the queue
+     * @param newTime - New time
+     */
+    function changeQueueExpirationTime(uint256 newTime) public onlyOwner {
+        queueExpirationTime = newTime;
+    }
+
+    /**
+     * @notice addZkCertificate issues a zkCertificate record by adding it to the Merkle tree
+     * @param leafIndex - leaf position of the zkCertificate in the Merkle tree
+     * @param zkCertificateHash - hash of the zkCertificate record leaf
+     * @param merkleProof - Merkle proof of the zkCertificate record leaf being free
+     */
+    function addZkCertificate(
+        uint256 leafIndex,
+        bytes32 zkCertificateHash,
+        bytes32[] memory merkleProof
+    ) public {
+        // since we are adding a new zkCertificate record, we assume that the leaf is of zero value
+        bytes32 currentLeafHash = ZERO_VALUE;
+        require(
+            _GuardianRegistry.isWhitelisted(msg.sender),
+            'ZkCertificateRegistry: not a Guardian'
+        );
+
+        require(
+            ZkCertificateToGuardian[zkCertificateHash] == address(0),
+            'ZkCertificateRegistry: zkCertificate already exists'
+        );
+
+        require(
+            checkZkCertificateHashInQueue(zkCertificateHash),
+            'ZkCertificateRegistry: zkCertificate is not in turn'
+        );
+
+        _changeLeafHash(
+            leafIndex,
+            currentLeafHash,
+            zkCertificateHash,
+            merkleProof
+        );
+        ZkCertificateToGuardian[zkCertificateHash] = ZkCertificateHashToCommitedGuardian[zkCertificateHash];
+        currentQueuePointer = ZkCertificateHashToIndexInQueue[zkCertificateHash] + 1;
+        emit zkCertificateAddition(zkCertificateHash, ZkCertificateToGuardian[zkCertificateHash], leafIndex);
+    }
+
+    /**
+     * @notice revokeZkCertificate removes a previously issued zkCertificate from the registry by setting the content of the merkle leaf to zero.
+     * @param leafIndex - leaf position of the zkCertificate in the Merkle tree
+     * @param zkCertificateHash - hash of the zkCertificate record leaf
+     * @param merkleProof - Merkle proof of the zkCertificate record being in the tree
+     */
+    function revokeZkCertificate(
+        uint256 leafIndex,
+        bytes32 zkCertificateHash,
+        bytes32[] memory merkleProof
+    ) public {
+        // since we are deleting the content of a leaf, the new value is the zero value
+        bytes32 newLeafHash = ZERO_VALUE;
+        require(
+            ZkCertificateToGuardian[zkCertificateHash] == msg.sender,
+            'ZkCertificateRegistry: not the corresponding Guardian'
+        );
+        require(
+            checkZkCertificateHashInQueue(zkCertificateHash),
+            'ZkCertificateRegistry: zkCertificate is not in turn'
+        );
+        _changeLeafHash(leafIndex, zkCertificateHash, newLeafHash, merkleProof);
+        // update the valid index
+        merkleRootValidIndex = merkleRoots.length - 1;
+        currentQueuePointer = ZkCertificateHashToIndexInQueue[zkCertificateHash] + 1;
+        emit zkCertificateRevocation(zkCertificateHash, ZkCertificateToGuardian[zkCertificateHash], leafIndex);
+        ZkCertificateToGuardian[zkCertificateHash] = address(0);
+
+    }
+
+    /** @notice Register a zkCertificate to the queue
+     * @param zkCertificateHash - hash of the zkCertificate record leaf
+     */
+    function registerToQueue(bytes32 zkCertificateHash) public {
+        require(
+            _GuardianRegistry.isWhitelisted(msg.sender),
+            'ZkCertificateRegistry: not a Guardian'
+        );
+        // we need to determine the time until which the Guardian needs to add/revoke the zkCertificate after registration to the queue
+        uint256 expirationTime;
+        // if the pointer is one slot after the end of the queue
+        // this means there is no other ZkCertificate pending, so the Guardian has queueExpirationTime from current time
+        // the strict inequality should never happen
+        if (currentQueuePointer >= ZkCertificateQueue.length) {
+            expirationTime = block.timestamp + queueExpirationTime;
+        // in the other case there is some other ZkCertificate pending
+        // the Guardian has queueExpirationTime after the time of the last registered ZkCertificate
+        } else {
+            expirationTime = ZkCertificateHashToQueueTime[ZkCertificateQueue[ZkCertificateQueue.length - 1]] + queueExpirationTime;
+        }
+        // we register the time and push the zkCertificateHash to the queue
+        ZkCertificateHashToQueueTime[zkCertificateHash] = expirationTime;
+        ZkCertificateHashToIndexInQueue[zkCertificateHash] = ZkCertificateQueue.length;
+        ZkCertificateHashToCommitedGuardian[zkCertificateHash] = msg.sender;
+        ZkCertificateQueue.push(zkCertificateHash);
+    }
+
+    function checkZkCertificateHashInQueue(bytes32 zkCertificateHash) public view returns (bool) {
+        uint256 index = ZkCertificateHashToIndexInQueue[zkCertificateHash];
+        // if the queue pointer points to the zkCertificateHash then the operation can proceed
+        require(index >= currentQueuePointer, 'ZkCertificateRegistry: pointer has already passed this zkCertificateHash');
+        if (index == currentQueuePointer) {
+            return true;
+        // if the expiration time of the previous zkCertificateHash has passed
+        // the operation can proceed
+        } else {
+            bytes32 previousZkCertificateHash = ZkCertificateQueue[index - 1];
+            return ZkCertificateHashToQueueTime[previousZkCertificateHash] < block.timestamp;
+        }
+    }
+
+    /** @notice Function change the leaf content at a certain index
+     * @param index the index of the overwritten leaf in the last level
+     * @param currentLeafHash the current content of the leaf
+     * @param newLeafHash the new content we want to write into the leaf
+     * @param merkleProof the merkle sibling path
+     **/
+    function _changeLeafHash(
+        uint256 index,
+        bytes32 currentLeafHash,
+        bytes32 newLeafHash,
+        bytes32[] memory merkleProof
+    ) internal {
+        require(
+            validate(merkleProof, index, currentLeafHash, merkleRoot()),
+            'merkle proof is not valid'
+        );
+        // we update the merkle tree accordingly
+        bytes32 newMerkleRoot = compute(merkleProof, index, newLeafHash);
+        merkleRoots.push(newMerkleRoot);
+        merkleRootIndex[newMerkleRoot] = merkleRoots.length - 1;
+    }
+
+    /**
+     * @notice Hash 2 uint256 values
+     * @param _left - Left side of hash
+     * @param _right - Right side of hash
+     * @return hash result
+     */
+    function hashLeftRight(
+        bytes32 _left,
+        bytes32 _right
+    ) public pure returns (bytes32) {
+        return PoseidonT3.poseidon([_left, _right]);
+    }
+
+        /**
+     * @notice function to validate a leaf content at a certain index with its merkle proof against a certain merkle root
+     */
+    function validate(
+        bytes32[] memory merkleProof,
+        uint256 index,
+        bytes32 leafHash,
+        bytes32 _merkleRoot
+    ) internal view returns (bool) {
+        return (compute(merkleProof, index, leafHash) == _merkleRoot);
+    }
+
+    /**
+     Function to validate a leaf value inside the verkle tree
+     @param verkleProof - array of the G1 points representing the proofs
+     @param verkleCommitments - array of the G1 points representing the polynomial commitments, the last one should be the G2 point with verkle root as x coordinate
+     @param index - the index of the leaf in the last level of the tree
+     @param leafValue - the value of the leaf
+     @param _verkleRoot - the verkle root
+     */
+    function validateVT(
+        Pairing.G1Point[] memory verkleProof,
+        Pairing.G1Point[] memory verkleCommitments,
+        uint256 index,
+        bytes32 leafValue,
+        bytes32 _verkleRoot
+    ) public view returns (bool) {
+        require(verkleProof.length == verkleCommitments.length, "Invalid verkle proof length");
+        require(bytes32(verkleCommitments[verkleCommitments.length - 1].X) == _verkleRoot, "Invalid verkle root");
+        // we check the polynomial for each level of the tree
+        // here 0 is the lowest level and d is the root level
+        /* at iteration d with current index, current leafValue we have:
+        - Lagrange polynomial p interpolated at the following leafWidth points:
+        p(0), p(1), ..., p(leafWidth - 1) is chunk of length leafWidth with values of leaves starting at index "(index - index % leafWidth)"
+        Then [p(s)]_1 is verkleCommitment[d]
+        - Quotient polynomial: q(x) = (p(x) - current leafValue) / (x - current index)
+        Then [q(s)]_1 is verkleProof[d]
+        - The function verifyKZG verifies that (in a different equivalent form):
+        e(verkleCommitment[d], G2.g) == e(verkleProof[d], [s]_2 - [current index]_2)
+        - Afterwards we change the current index and current leafValue to prepare for the next level
+        */
+
+        for (uint d = 0; d < treeDepth; d++) {
+            uint polynomialIndex = index % leafWidth;
+            require(verifyKZG(verkleCommitments[d].X, verkleCommitments[d].Y, verkleProof[d].X, verkleProof[d].Y, polynomialIndex, uint256(leafValue)), "KZG verification failed");
+            leafValue = bytes32(verkleCommitments[d].X);
+            index = index / leafWidth;
+        }
+        return true;
+    }
+
+    function compute(
+        bytes32[] memory merkleProof,
+        uint256 index,
+        bytes32 leafHash
+    ) internal view returns (bytes32) {
+        require(index < treeSize, '_index bigger than tree size');
+        require(merkleProof.length == treeDepth, 'Invalid _proofs length');
+
+        for (uint256 d = 0; d < treeDepth; d++) {
+            if ((index & 1) == 1) {
+                leafHash = hashLeftRight(merkleProof[d], leafHash);
+            } else {
+                leafHash = hashLeftRight(leafHash, merkleProof[d]);
+            }
+            index = index >> 1;
+        }
+        return leafHash;
+    }
+
+    function verifyMerkleRoot(bytes32 _merkleRoot) public view returns (bool) {
+        uint _merkleRootIndex = merkleRootIndex[_merkleRoot];
+        return _merkleRootIndex >= merkleRootValidIndex;
+    }
+
+    // function to return the time parameters of the period where one is allowed to add ZkCertificate
+    function getTimeParameters(bytes32 zkCertificateHash) public view returns (uint, uint) {
+        uint expirationTime = ZkCertificateHashToQueueTime[zkCertificateHash];
+        require(expirationTime != 0, "ZkCertificateRegistry: zkCertificate is not in the queue");
+        uint indexInQueue = ZkCertificateHashToIndexInQueue[zkCertificateHash];
+        require(indexInQueue >= currentQueuePointer, 'ZkCertificateRegistry: pointer has already passed this zkCertificateHash');
+        if (currentQueuePointer == indexInQueue || indexInQueue == 0) {
+            return (block.timestamp, expirationTime);
+        } else {
+            uint startTime = ZkCertificateHashToQueueTime[ZkCertificateQueue[indexInQueue - 1]];
+            return (startTime, expirationTime);
+        }
+    }
+}
