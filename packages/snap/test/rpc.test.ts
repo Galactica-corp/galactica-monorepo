2cdaec6d0622a63cc9ed945bef2b347792734159
anhdungle <anhdungle634@gmail.com>
2024-09-25 12:58:05 +0800

    snap api fix

--- packages/snap/test/rpc.test.ts
+++ packages/snap/test/rpc.test.ts
@@ -7,6 +7,7 @@ import type {
   ZkCertProof,
   ZkCertRegistered,
   ZkCertSelectionParams,
+  ProverData,
 } from '@galactica-net/snap-api';
 import {
   RpcMethods,
@@ -108,7 +109,7 @@ describe('Test rpc handler function', function () {
 
   before(function () {
     // prepare URL to fetch provers from
-    const { prover } = testZkpParams; // we know it is a prover
+    const prover = testZkpParams.prover as ProverData; // we know it is a prover
     fetchMock.get(testProverURL + subPathWasm, JSON.stringify(prover.wasm));
     fetchMock.get(
       testProverURL + subPathZkeyHeader,
@@ -712,7 +713,7 @@ describe('Test rpc handler function', function () {
 
       // Merkle proof should have been updated and stored
       expect(fetchMock.calls().length).to.equal(
-        2 + testZkpParams.prover.zkeySections.length,
+        2 + (testZkpParams.prover as ProverData).zkeySections.length,
       );
     });
 
@@ -742,7 +743,7 @@ describe('Test rpc handler function', function () {
         'Prover data hash does not match hash in ProverLink.',
       );
       expect(fetchMock.calls().length).to.equal(
-        2 + testZkpParams.prover.zkeySections.length,
+        2 + (testZkpParams.prover as ProverData).zkeySections.length,
       );
     });
 
