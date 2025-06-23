3137d830e6248fde6f87257a8de88fa62acbf387
Frederik Linker <frederik@codelogic.dev>
2025-06-23 15:15:11 +0200

    zkCert: small readme fix

--- packages/zk-certificates/scripts/dev/verifySC.ts
+++ packages/zk-certificates/scripts/dev/verifySC.ts
@@ -3,13 +3,11 @@ import hre from 'hardhat';
  * Verifies a smart contract after deployment.
  */
 async function main() {
-  const SCAddress = '0x6426f716BB18D1F6767766AecCA65349866954A3';
+  const SCAddress = '0xebB99364d4615B7667560f89e28fBf56bE82BfE9';
   const constructorInputs = [
-    '0xC578b092204f48D34339fa76f8A2719817298978',
-    'https://quicknode.quicknode-ipfs.com/ipfs/QmTWZSCpQwzEcjCa7sriwPXoJHjC9dTyy7P94cT4KkBvTm',
-    '0xC578b092204f48D34339fa76f8A2719817298978',
-    'Genesis SBT',
-    'XNET',
+    '0x4d16D597BA3D144d62A557593443B0468E1A1D1a',
+    '32',
+    'Twitter ZkCertificate Registry',
   ];
 
   console.log(`verifying contract at address ${SCAddress}`);
@@ -19,6 +17,9 @@ async function main() {
   await hre.run('verify:verify', {
     address: SCAddress,
     constructorArguments: constructorInputs,
+    libraries: {
+      PoseidonT3: '0x60337CE680b20D1fbc8b1205A5A8881F6ECdC2Db',
+    },
   });
 }
 
