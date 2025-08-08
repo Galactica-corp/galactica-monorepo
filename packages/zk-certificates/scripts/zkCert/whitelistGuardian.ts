c12f878ad385e2b63edea9179769e2f224f17372
Frederik Linker <frederik@codelogic.dev>
2025-08-08 11:36:01 +0200

    zkCert: fixed guardian whitelisting script

--- packages/zk-certificates/scripts/zkCert/whitelistGuardian.ts
+++ packages/zk-certificates/scripts/zkCert/whitelistGuardian.ts
@@ -2,33 +2,24 @@
 import { ethers } from 'hardhat';
 
 import { decompressEddsaPubKey } from '../../lib';
-import {
-  whitelistGuardian,
-  // whitelistSignerGuardian,
-} from '../deploy/deploymentSteps/whitelistGuardian';
+import { GuardianRegistry } from '../../typechain-types';
 
 /**
  * Script for adding a KYC center to the KYC center registry.
  */
 async function main() {
   // parameters
-  const [deployer] = await ethers.getSigners();
-  const centerRegistryAddr = '0x1263E38AFb0449932F1aa0e108a009d895190Ee6';
-  const metadataURL = 'ipfs://QmTdHoVA9gLA1Mk6Csfd2SxMkEdbuSu7xK8dDxUdFVCunx';
-  console.log(`guardian address is ${await deployer.getAddress()}`);
-  // await whitelistSignerGuardian(deployer, centerRegistryAddr, deployer, metadataURL);
+  const centerRegistryAddr = '0x466bF1463F380C903CA69C2FeEF419824D8eA4d7';
+  const metadataURL = 'https://bafybeigkjmb3aeqbfdcqwlbirhbbylce3b5rjttdp2fefnjjuwet5glpxe.ipfs.dweb.link';
 
+  const guardianAddress = '0x43532C0b134E2173c08b386AA17ec8E8e0ecadd0';
   const compressedPubKey =
     '1c896106914117927552a77aef0a771af8376435adc3fd51f819d34fbfda93a1';
-  const guardianAddress = '0x43532C0b134E2173c08b386AA17ec8E8e0ecadd0';
   const pubKey = await decompressEddsaPubKey(compressedPubKey);
-  await whitelistGuardian(
-    deployer,
-    centerRegistryAddr,
-    guardianAddress,
-    pubKey,
-    metadataURL,
-  );
+  const guardianRegistry = await ethers.getContractAt('GuardianRegistry', centerRegistryAddr) as unknown as GuardianRegistry;
+  const tx = await guardianRegistry.grantGuardianRole(guardianAddress, pubKey, metadataURL);
+  await tx.wait();
+  console.log('Guardian whitelisted, tx hash:', tx.hash);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
