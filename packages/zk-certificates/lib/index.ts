9905c7592f5f9c08f9bd115269f0dd9aee80c081
Alexander Chori <alexander@occam.fi>
2025-09-02 15:17:38 +0200

    Extract proof generation methods to zk-certificates

--- packages/zk-certificates/lib/index.ts
+++ packages/zk-certificates/lib/index.ts
@@ -1,4 +1,6 @@
-/* Copyright (C) 2023 Galactica Network. This file is part of zkKYC. zkKYC is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. zkKYC is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>. */
+/*
+ * Copyright (C) 2025 Galactica Network. This file is part of zkKYC. zkKYC is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. zkKYC is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
 /**
  * Index defining exports of lib functions to other packages using this module
  */
@@ -16,6 +18,8 @@ export * from './shamirTools';
 export * from './sparseMerkleTree';
 export * from './zkCertificate';
 export * from './zkCertificateDataProcessing';
+export * from './proofs';
+export * from './merkle';
 export type { getContentFields } from '@galactica-net/galactica-types';
 export type { ZkCertStandard } from '@galactica-net/galactica-types';
 export { KnownZkCertStandard } from '@galactica-net/galactica-types';
