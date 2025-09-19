5dc19d3a04b7acc54d7a9db9144a5501553dba6b
Frederik Linker <frederik@codelogic.dev>
2025-09-19 17:25:38 +0200

    zkCert: foundation for upgradable contracts in ignition

---
+++ packages/zk-certificates/ignition/modules/UpgradableProxy.m.ts
@@ -0,0 +1,58 @@
+// This setup uses Hardhat Ignition to manage smart contract deployments.
+// Learn more about it at https://hardhat.org/ignition
+
+/**
+ * Using the Transparent Proxy pattern from OpenZeppelin: https://docs.openzeppelin.com/contracts/5.x/api/proxy#TransparentUpgradeableProxy
+ * Documentation how to use it with Hardhat Ignition: https://hardhat.org/ignition/docs/guides/upgradeable-proxies
+ *
+ * @param module - The IgnitionModuleBuilder instance.
+ * @param contractName - The name of the contract to deploy.
+ * @param initCallArgs - The arguments to pass to the initialize function of the contract.
+ * @returns The upgradable contract and the proxy contracts.
+ */
+export function defineUpgradableProxy(
+  module: any /* IgnitionModuleBuilder*/,
+  contractName: string,
+  initCallArgs: any[],
+) {
+  // Deploying the staking logic contract
+  const implementation = module.contract(contractName, [], {
+    id: `ProxyImplementation${contractName}`,
+  });
+
+  // Call to initialize the staking contract when the proxy is deployed
+  const encodedInitCall = module.encodeFunctionCall(
+    implementation,
+    'initialize',
+    initCallArgs,
+  );
+
+  // Proxy infrastructure
+  const proxyAdminOwner = module.getAccount(0); // deployer by default, can be changed later
+
+  const proxy = module.contract(
+    'TransparentUpgradeableProxy',
+    [implementation, proxyAdminOwner, encodedInitCall],
+    { id: `TransparentUpgradeableProxy${contractName}` },
+  );
+
+  const proxyAdminAddress = module.readEventArgument(
+    proxy,
+    'AdminChanged',
+    'newAdmin',
+  );
+
+  const proxyAdmin = module.contractAt('ProxyAdmin', proxyAdminAddress, {
+    id: `ProxyAdmin${contractName}`,
+  });
+
+  // contract using the proxy
+  const upgradableContract = module.contractAt(contractName, proxy, {
+    id: `UpgradableContract${contractName}`,
+  });
+
+  return {
+    upgradableContract,
+    proxyContracts: { proxyAdmin, proxy },
+  };
+}
