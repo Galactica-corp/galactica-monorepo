91820f43aecd8fb48a966698bfc996084a7a9117
Frederik Linker <frederik@codelogic.dev>
2025-09-25 22:06:27 +0200

    zkCert: fixed hardhat config without mainnet key loaded

--- packages/zk-certificates/hardhat.config.ts
+++ packages/zk-certificates/hardhat.config.ts
@@ -78,7 +78,7 @@ const config: HardhatUserConfig = {
     },
     galacticaMainnet: {
       url: `https://galactica-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
-      accounts: [process.env.GnetMainnetDeployerPrivateKey ?? ''],
+      accounts: process.env.GnetMainnetDeployerPrivateKey ? [process.env.GnetMainnetDeployerPrivateKey] : [],
     },
   },
   etherscan: {
